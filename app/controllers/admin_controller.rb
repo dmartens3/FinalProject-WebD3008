ad2adc1945cd14359c65adec0e366106e29168a8
dmartens3 <dmartens3@rrc.ca>
2024-03-27 16:28:55 -0500

    Rubocop autocorrected a bunch of little things

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 class AdminController < ApplicationController
+  # Make sure that admins are authenticated
+  before_action :authenticate_admin!
 
-    # Make sure that admins are authenticated
-    before_action :authenticate_admin!
-
-    # Go to admin home page
-    def index
-    end
+  # Go to admin home page
+  def index; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "home#index"
+  root 'home#index'
 
   # send authenticated admin users to the admin home page instead of the regular home page.
   authenticated :admin_user do
-    root to: "admin#index", as: :admin_root
+    root to: 'admin#index', as: :admin_root
   end
 
   # I think this links http://127.0.0.1:3000/admin to the admin home page.
-  get "admin" => "admin#index"
+  get 'admin' => 'admin#index'
 end
