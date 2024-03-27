6a8b44f24c8e932417e98f56534661eebdbaea79
dmartens3 <dmartens3@rrc.ca>
2024-03-27 16:58:46 -0500

    Ran: rails g scaffold admin::categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
