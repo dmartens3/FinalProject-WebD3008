8cae26de7461b0091e81a7a3ce9ee67957eefa45
dmartens3 <dmartens3@rrc.ca>
2024-03-27 12:35:03 -0500

    Added comments to remember what the code does

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,9 @@
 class AdminController < ApplicationController
+
+    # Make sure that admins are authenticated
     before_action :authenticate_admin!
+
+    # Go to admin home page
     def index
     end
 end
