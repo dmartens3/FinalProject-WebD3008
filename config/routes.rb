2bf1fa462e67df81856a2ff7a0b668e1959e5b8c
dmartens3 <dmartens3@rrc.ca>
2024-03-27 19:38:40 -0500

    Ran: rails g scaffold admin::product

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   namespace :admin do
+    resources :products
     resources :categories
   end
   devise_for :admins
---
+++ db/migrate/20240328003743_create_admin_products.rb
@@ -0,0 +1,11 @@
+class CreateAdminProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :admin_products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
