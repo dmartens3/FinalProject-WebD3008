966b9bd36184a6285dd9e43fd03359e58450f555
dmartens3 <dmartens3@rrc.ca>
2024-03-27 20:12:47 -0500

    Adjusted admin product table to display properly

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  #allow an image to be attached and returns a thumbnail when you ask for the thumb variant.
+  has_many_attached :images do |i|
+    i.variant :thumb, resize_to_limit: [50, 50]
+  end
 end
