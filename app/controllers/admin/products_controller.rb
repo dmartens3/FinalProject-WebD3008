8ef7fa2d3ae234b7da73f34fe66f238a64ca893d
dmartens3 <dmartens3@rrc.ca>
2024-03-27 22:30:05 -0500

    Much tinkering for little pic of product on list

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -65,6 +65,6 @@ class Admin::ProductsController < AdminController
 
     # Only allow a list of trusted parameters through.
     def admin_product_params
-      params.require(:product).permit(:name, :description, :price)
+      params.require(:product).permit(:name, :description, :price, images: [])
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,33 @@
 class Product < ApplicationRecord
   #allow an image to be attached and returns a thumbnail when you ask for the thumb variant.
-  has_many_attached :images do |i|
-    i.variant :thumb, resize_to_limit: [50, 50]
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
   end
 end
+
+# class OctoCat < ApplicationRecord
+#   has_one_attached :avatar
+#   validate :avatar_format
+#   def resize_image
+#       resized_image = MiniMagick::Image.read(avatar.download)
+#       resized_image = resized_image.resize "400x400"
+#       v_filename = avatar.filename
+#       v_content_type = avatar.content_type
+#       avatar.purge
+#       avatar.attach(io: File.open(resized_image.path), filename:  v_filename, content_type: v_content_type)
+#   end
+#   private
+#   def avatar_format
+#    return unless avatar.attached?
+#    if avatar.blob.content_type.start_with? 'image/'
+#      if avatar.blob.byte_size > 10.megabytes
+#        errors.add(:avatar, 'size needs to be less than 10MB')
+#        avatar.purge
+#      else
+#        resize_image
+#      end
+#     else
+#       avatar.purge
+#       errors.add(:avatar, 'needs to be an image')
+#     end
+#   end
