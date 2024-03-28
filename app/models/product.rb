10b534be18708d8d1583ad774255347f6cd2db4c
dmartens3 <dmartens3@rrc.ca>
2024-03-28 13:02:39 -0500

    Changed image.variant(:small) to 200x200

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   #allow an image to be attached and returns a thumbnail when you ask for the thumb variant.
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [50, 50]
-    attachable.variant :medium, resize_to_limit: [250, 250]
+    attachable.variant :small, resize_to_limit: [200, 200]
   end
 
   # Relationships
